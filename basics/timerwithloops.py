a31f155a113dfaddcca87a81dcaa71620ee649ba
tsharanya <tsharanya@sapplica.com>
2018-11-23 16:13:48 +0530

    Loops

---
+++ basics/timerwithloops.py
@@ -0,0 +1,16 @@
+import time
+import sys
+
+def countdown(n) :
+    while n > 0:
+        mins, secs = divmod(n, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        sys.stdout.write('\r'+timeformat)
+        sys.stdout.flush()
+        # print(timeformat)
+        time.sleep(1)
+        n -= 1
+    if n == 0:
+        print('BLAST OFF!')
+
+countdown(120)
